c5e99805953b84cd3dd36d706131e9daf9902135
Marleen Nabil <eng.marleennabil@gmail.com>
2026-06-09 21:23:18 +0300

    add some more info

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -4,10 +4,20 @@ export default function ProfileModal({isOpen, onClose, button}){
         1:{
             title: "First Stop",
             text:"Content for button 1",
-        }
+        },
+        2:{
+            title: "Second Stop",
+            text:"Content for button 2",
+        },
+        3:{
+            title:"Third Stop",
+            text:"Content for button 3",
+        },
         
-    }
+    };
     return(
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
