b4a1f42c56ff5c91b25b8bfc36d05d5e2dc5745c
Marleen Nabil <eng.marleennabil@gmail.com>
2026-06-13 23:38:41 +0300

    edit

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -5,12 +5,12 @@ export default function ProfileModal({ isOpen, onClose, button }) {
         1: {
             title: "رعمسيس",
             text: "  كانت رعمسيس هي أول نقطة إنطلاق",
-            audio: "/audio/first.mp3",
+            // audio: "/audio/first.mp3",
         },
         2: {
             title: "Second Stop",
             text: "Content for button 2",
-            
+
         },
         3: {
             title: "Third Stop",
@@ -21,17 +21,16 @@ export default function ProfileModal({ isOpen, onClose, button }) {
         <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
             <div className="bg-white rounded-lg w-100 max-h-80 overflow-y-auto p-6">
                 <div>
-                <div className="flex justify-between mb-4" >
-                    <h2 className="text-2xl font-bold">{content[button]?.title}</h2>
-                    <button onClick={onClose} className=" flex justify-end font-semibold text-2xl">x</button>
-                </div>
-                <div className="flex items-center gap-4">
+                    <div className="flex justify-between mb-4" >
+                        <h2 className="text-2xl font-bold">{content[button]?.title}</h2>
+                        <button onClick={onClose} className=" flex justify-end font-semibold text-2xl">x</button>
+                    </div>
+                    {/* <div className="flex items-center gap-4">
                     <Sound src={content[button]?.audio}/>
                     <button onClick={onClose}>X</button>
-                </div>
+                </div> */}
                 </div>
                 <p>{content[button]?.text}</p>
-
             </div>
 
         </div>
--- src/components/atoms/Sound.jsx
+++ src/components/atoms/Sound.jsx
@@ -1,8 +1,8 @@
-import { AiOutlineSound } from "react-icons/ai";
+
 export default function Sound (){
     return(
         <div>
-            <button className="cursor-pointer text-3xl text-blue-500"><AiOutlineSound/></button>
+            <button className="cursor-pointer text-3xl text-blue-500"></button>
             
         </div>
     )
