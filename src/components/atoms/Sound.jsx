2494fb92f54c2e7459b1c5dbf4f884413c9c78a2
Marleen Nabil <eng.marleennabil@gmail.com>
2026-06-14 17:45:16 +0300

    another edit in the audio play and pause

--- src/components/atoms/Sound.jsx
+++ src/components/atoms/Sound.jsx
@@ -1,9 +1,18 @@
 import { AiOutlineSound } from "@react-icons/all-files/ai/AiOutlineSound"
 export default function Sound({ audioRef }) {
+    const toggleSound = () => {
+        if (!audioRef.current) return;
+
+        if (audioRef.current.paused) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+    };
 
     return(
         <div>
-            <button onClick={()=>audioRef.current.play()} className="cursor-pointer text-3xl text-blue-500"><AiOutlineSound/></button>
+            <button onClick={toggleSound} className="cursor-pointer text-3xl text-blue-500"><AiOutlineSound/></button>
         
         </div>
     )
