0bca4aa3af240fb01e688d11681762855a92d3dc
Marleen Nabil <eng.marleennabil@gmail.com>
2026-06-18 19:23:25 +0300

    add some text, audio

--- src/layout/TopNavBar.jsx
+++ src/layout/TopNavBar.jsx
@@ -3,17 +3,17 @@ export default function TopNavBar() {
     return (
         <nav className="hidden sticky sm:flex text-zinc-200 bg-linear-to-r from-amber-950 to-amber-700 px-2 py-3 justify-between gap-2">
             <div>
-                <img src="/stMoses.jpg" alt="" className="flex w-20 h-20 rounded-full" />
+                <img src="/stMoses.jpg" alt="" className="flex w-15 h-15 rounded-full" />
             </div>
             
             
             <div >
-                <Link to="aboutmoses" className="m-4"> شخصية موسى النبي</Link>
-                <Link to="journey" className="m-4">رحلة الإيمان</Link>
-                <Link to="reference" className="m-4">المراجع</Link>
+                <Link to="aboutmoses" className="m-4 font-normal text-xl"> شخصية موسى النبي</Link>
+                <Link to="journey" className="m-4 font-normal text-xl">رحلة الإيمان</Link>
+                <Link to="reference" className="m-4 font-normal text-xl">المراجع</Link>
             </div>
             <div>
-                <Link to="concept">الهدف</Link>
+                <Link to="concept" className="m-4 font-normal text-xl">الهدف</Link>
             </div>
 
         </nav>
--- src/pages/References.jsx
+++ src/pages/References.jsx
@@ -1,15 +1,24 @@
 import PageTitle from "../components/atoms/PageTitle";
 export default function References() {
     return (
-        <div>
+        <div className="w-full min-h-screen bg-linear-to-r from-amber-500 to-blue-500">
             <div className="flex justify-center -z-10 m-3">
                 <PageTitle title="المراجع" />
             </div>
             <div className="flex justify-end-safe grid-cols-2">
-                <img src="/stMosesPic.jpeg" alt="" className="p-2 rounded-3xl max-w-3xl max-h-screen mr-3" />
-                <div className="flex justify-items mr-4">
+                <img src="/stMosesPic.jpeg" alt="" className="p-2 rounded-3xl w-3xl max-h-screen mr-3" />
+                <div className="flex justify-items mr-4 text-right leading-8" dir="rtl">
                     <ul>
-                        <li>أوريجانوس، النصوص المسيحية، عظات على سفر الخروج، ترجمة مريم رشاد حليم و جرجس بشرى، مركز باناريون للتراث الآبائي+ </li>
+                        <li>+ كتاب المقدس </li>
+                        <li>+ أوريجانوس، النصوص المسيحية، عظات على سفر الخروج، ترجمة مريم رشاد حليم و جرجس بشرى، مركز باناريون للتراث الآبائي </li>
+                        <li>+ تادرس يعقوب ملطي، من تفسير الآباء الأولين، سفر الخروج، كنيسة الشهيد مارجرجس سبورتنج </li>
+                        <li>+ القمص تادرس يعقوب ملطي – تفسير سفر الخروج</li>
+                        <li>+ تفسير القمص تادرس يعقوب ملطي لسفر خروج والعدد</li>
+                        <li>+ دائرة المعارف الكتابية (مدخل: فم الحيروث، مارة)</li>
+                        <li>+ مقدمات الأسفار وخرائط الكتاب المقدس الصادرة عن St-Takla.org </li>
+                        <li>+ أطلس الكتاب المقدس الصادر عن دار الثقافة القبطية</li>
+                        <li>+ محاضرات ابونا كيرلس رشدى </li>
+                        <li>+ محاضرات ابونا مرقس فوزى</li>
                     </ul>
                 </div>
 
